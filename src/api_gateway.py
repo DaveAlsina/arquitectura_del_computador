9927b72a819d78bf4e7e78f91a01481e6ac62f3d
DaveAlsina <davealsina@gmail.com>
2023-11-30 23:42:11 -0500

    Update
    Renamed files, and integrated api gateway changes

--- src/api_gateway.py
+++ src/api_gateway.py
@@ -1,32 +1,69 @@
-import request_mediator
-from typing import Union
+#import request_mediator
+import api_models
 
-from fastapi import FastAPI
+import fastapi as fapi
+
+
+app = fapi.FastAPI()
+
+
+
 class APIGateway():
-    app = FastAPI()
+
+    User = api_models.User()
+    Zone = api_models.Zone()
+    Measurement = api_models.Measurement()
+
+    @app.get("/")
+    def read_root(self):
+        return {"conectado :P"}
+
     def __init__(self) -> None:
         print("\nAPIGateway: Inicializando...")
-        self.request_mediator = request_mediator.RequestMediator()
+   
 
-    @app.get("/login/{user}")
-    @app.get("/login/{password}")
-    def log_in(self, user: str, password: str):
-        print(f"\nAPIGateway: log_in user: {user}, password: {password}")
-        self.request_mediator.log_in(user, password)
+    @app.post("/register")
+    def register(self,user: User):
+        if user.username in users:
+            raise fapi.HTTPException(status_code=400, detail="Username already exists")
+        users[user.username] = user
+        return {"message": "User registered successfully"}
 
-    @app.get("/login/{user}")
-    @app.get("/login/{password}")
-    def create_user(self, user: str, password: str):
-        print(f"\nAPIGateway: create_user user: {user}, password: {password}")
-        self.request_mediator.create_user(user, password)
+    @app.post("/login")
+    def login(self, user: User):
+        if user.username not in users or users[user.username].password != user.password:
+            raise fapi.HTTPException(status_code=400, detail="Invalid username or password")
+        return {"message": "Logged in successfully"}
 
-        
-    @app.get("/Zones/{zone}")
-    def create_zone(self, zone: dict):
-        print(f"\nAPIGateway: create_zone zone: {zone}")
-        self.request_mediator.create_zone(zone)
+    @app.post("/zone")
+    def create_zone(self,zone: Zone):
+        if zone.name in zones:
+            raise fapi.HTTPException(status_code=400, detail="Zone already exists")
+        zones[zone.name] = []
+        return {"message": "Zone created successfully"}
 
-    @app.get("/Measurement/{measurement}")
-    def add_measurement(self, measurement: dict):
-        print(f"\nAPIGateway: add_measurement measurement: {measurement}")
-        self.request_mediator.add_measurement(measurement)
+    @app.post("/measurement")
+    def add_measurement(self,measurement: Measurement):
+        if measurement.zone not in zones:
+            raise fapi.HTTPException(status_code=400, detail="Zone does not exist")
+        zones[measurement.zone].append(measurement.value)
+        return {"message": "Measurement added successfully"}
+
+    @app.get("/measurement/{zone}")
+    def get_measurement(self,zone: str):
+        if zone not in zones:
+            raise fapi.HTTPException(status_code=400, detail="Zone does not exist")
+        return {"measurements": zones[zone]}
+
+    @app.get("/zones")
+    def get_zones(self):
+        return {"zones": list(zones.keys())}
+
+
+
+
+
+
+
+    
+
