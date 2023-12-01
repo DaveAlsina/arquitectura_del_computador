e0ea5fcc258a00b4be2a91fff5c6769ceb23b994
Arevalinchy <75390468+Arevalinchy@users.noreply.github.com>
2023-11-30 23:13:07 -0500

    Add files via upload

---
+++ api_models.py
@@ -0,0 +1,24 @@
+from typing import Union
+from pydantic import BaseModel
+import fastapi as fapi
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional, Dict
+
+app = fapi.FastAPI()
+
+# Pydantic models
+class User(BaseModel):
+    username: str
+    password: str
+
+class Zone(BaseModel):
+    name: str
+
+class Measurement(BaseModel):
+    zone: str
+    value: float
+
+
+
+
